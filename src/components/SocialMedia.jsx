5c9fe5bf8c358e7fb84cfa78bd3998e12dacaaa0
ClintTheEngineer <81938100+ClintTheEngineer@users.noreply.github.com>
2022-04-06 13:23:05 -0400

    Add files via upload

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { BsGithub } from 'react-icons/bs';
+
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <BsGithub />
+    </div>
+   
+  </div>
+);
+
+export default SocialMedia;
